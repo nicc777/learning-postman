67bd2b570a37a6bca1f75f52eb36d60098918a79
Nico Coetzee <nicc777@gmail.com>
2020-09-21 04:14:06 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# learning-postman
-A demo API and documentation with the aim of learning Postman
+
+- [Postman Learnings via Demo App](#postman-learnings-via-demo-app)
+- [Requirements and Assumptions](#requirements-and-assumptions)
+- [Additional Resources](#additional-resources)
+- [Getting started](#getting-started)
+
+# Postman Learnings via Demo App
+
+A demonstration of how several technologies can work together. The main aim was to learn Postman, and specifically scripting in Postman.
+
+This repo contains all the scripts and config files to demonstrate some of the basic Postman scripting functionality through an actual running service with Authentication and Authorization.
+
+The lab design can be seen in the following diagram:
+
+![Lab Environment](./images/basic_design.png)
+
+# Requirements and Assumptions
+
+The demo was developed on a Linux system, but should work on most modern operating systems. Some additional steps or tweaks to steps may be required for some environments. 
+
+Basic Requirements:
+
+* Operating system that supports Python 3 and Postman
+* Python 3
+* Postman
+
+From an experience perspective, the following basic knowledge and experience should be sufficient to follow the examples:
+
+* Basic understanding of what [JSON](https://en.wikipedia.org/wiki/JSON) is
+* Basic understanding of what [REST](https://en.wikipedia.org/wiki/Representational_state_transfer) is
+* Familiarity with Python and [`pip`](https://realpython.com/what-is-pip/)
+* Comfortable on the command line
+* General understanding of [OAUTH2](https://oauth.net/2/) and [what it is](https://developer.okta.com/blog/2017/06/21/what-the-heck-is-oauth)
+* Some experience in [making REST calls with curl](https://www.codepedia.org/ama/how-to-test-a-rest-api-from-command-line-with-curl/) will also go a long 
+
+# Additional Resources
+
+* Auth0 References:
+  * [Which OAuth 2.0 Flow Should I Use?](https://auth0.com/docs/authorization/which-oauth-2-0-flow-should-i-use)
+  * [Call Your API Using the Client Credentials Flow](https://auth0.com/docs/flows/call-your-api-using-the-client-credentials-flow)
+* RFC's:
+  * [RFC 6749 - The OAuth 2.0 Authorization Framework](https://tools.ietf.org/html/rfc6749)
+* OpenAPI 3:
+  * [What is OpenAPI 3?](https://swagger.io/docs/specification/about/)
+  * [OpenAPI 3 Specification](https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md)
+  * [More examples and explanations of concepts](https://swagger.io/docs/specification/basic-structure/)
+* Python 3 Packages:
+  * [Connexion Documentation](https://connexion.readthedocs.io/en/latest/)
+  * [PyJWT Documentation](https://pyjwt.readthedocs.io/en/latest/index.html)
+
+# Getting started
+
+Please use the Wiki for detailed instructions (**_TODO_**)
---
+++ test-service/src/test_service/demo_service.py
@@ -0,0 +1,134 @@
+import connexion
+from datetime import datetime
+import os
+import traceback
+from six.moves.urllib.request import urlopen
+import json
+from jose import jwt
+from flask import _request_ctx_stack
+
+
+SPECIFICATION_DIR = os.getenv('SPECIFICATION_DIR', './')
+AUTH0_DOMAIN = 'dev-y0ottw4e.auth0.com'
+API_AUDIENCE = 'http://localhost:5000'
+ALGORITHMS = ["RS256"]
+
+
+def get_utc_timestamp(with_decimal: bool=False): 
+    epoch = datetime(1970,1,1,0,0,0) 
+    now = datetime.utcnow() 
+    timestamp = (now - epoch).total_seconds() 
+    if with_decimal: 
+        return timestamp 
+    return int(timestamp) 
+
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+
+class CertCache:
+
+    def __init__(self):
+        self.cert = None
+        self.cert_expires = 0
+
+    def _refresh_cert(self):
+        try:
+            print('CertCache: refreshing JWKS')
+            jsonurl = urlopen("https://"+AUTH0_DOMAIN+"/.well-known/jwks.json")
+            self.cert = json.loads(jsonurl.read())
+            self.cert_expires = get_utc_timestamp(with_decimal=False) + 3600
+        except:
+            print('EXCEPTION: {}'.format(traceback.format_exc()))
+
+    def get_cert(self):
+        now = get_utc_timestamp(with_decimal=False)
+        if now > self.cert_expires:
+            self._refresh_cert()
+        return self.cert
+
+
+cert_cache = CertCache()
+
+
+def decode_token(token):
+    payload = dict()
+    try:
+        # return jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        # jsonurl = urlopen("https://"+AUTH0_DOMAIN+"/.well-known/jwks.json")
+        # jwks = json.loads(jsonurl.read())
+        jwks = cert_cache.get_cert()
+        unverified_header = jwt.get_unverified_header(token)
+        rsa_key = {}
+        for key in jwks["keys"]:
+            if key["kid"] == unverified_header["kid"]:
+                rsa_key = {
+                    "kty": key["kty"],
+                    "kid": key["kid"],
+                    "use": key["use"],
+                    "n": key["n"],
+                    "e": key["e"]
+                }
+        if rsa_key:
+            try:
+                payload = jwt.decode(
+                    token,
+                    rsa_key,
+                    algorithms=ALGORITHMS,
+                    audience=API_AUDIENCE,
+                    issuer="https://"+AUTH0_DOMAIN+"/"
+                )
+            except jwt.ExpiredSignatureError:
+                raise AuthError({"code": "token_expired",
+                                "description": "token is expired"}, 401)
+            except jwt.JWTClaimsError:
+                raise AuthError({"code": "invalid_claims",
+                                "description":
+                                    "incorrect claims,"
+                                    "please check the audience and issuer"}, 401)
+            except Exception:
+                raise AuthError({"code": "invalid_header",
+                                "description":
+                                    "Unable to parse authentication"
+                                    " token."}, 401)
+
+            _request_ctx_stack.top.current_user = payload
+        raise AuthError({"code": "invalid_header",
+                        "description": "Unable to find appropriate key"}, 401)
+    except:
+        print('EXCEPTION: {}'.format(traceback.format_exc()))
+    return payload
+
+
+def readiness():
+    return { 'timestamp': get_utc_timestamp(with_decimal=False) }, 200
+
+
+def user_profile_get(token_info):
+    print('token_info={}'.format(token_info))
+    user_id = token_info['sub']
+    name = 'User 1'
+    email_address = 'user1@example.tld'
+    return { 'userId': user_id, 'name': name, 'emailAddress': email_address }, 200
+
+
+options = {"swagger_ui": False}
+if int(os.getenv('SWAGGER_UI', '1')) > 0:
+    options = {"swagger_ui": True}    
+app = connexion.FlaskApp(__name__, specification_dir=SPECIFICATION_DIR)
+app.add_api('demo-service.yaml', strict_validation=True)
+
+
+def run():
+    app.run(port=5000)
+
+if __name__ == '__main__':
+    run()
+
+
+
+
+# EOF
